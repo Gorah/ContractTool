59e39436d7e50fbbe6fd4d42f842dca8ebdd665b
Gorah <bartosz.kratochwil@hp.com>
2014-04-08 12:48:25 +0200

    added fields and modified rules to fix bugs

--- contract/model/DocxHireModel.java
+++ contract/model/DocxHireModel.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -27,6 +29,7 @@ public class DocxHireModel extends HireAbstractModel {
 
 	@Override
 	public void readDataFromFile() {
+		//Iterate through document elements to get data stored in rows.
 		for(int tabCount = 0; tabCount<tables.size(); tabCount++){
 			XWPFTable table = tables.get(tabCount);
 			List<XWPFTableRow> rows = table.getRows();
@@ -34,6 +37,21 @@ public class DocxHireModel extends HireAbstractModel {
 				XWPFTableRow row = rows.get(rowCount);
 				List<XWPFTableCell> cells = row.getTableCells();
 				
+				//Add model fields, that are not present on the source document
+				addField("contract_ref", "");
+				addField("eeid", "");
+				addField("signatory_name_req", "");
+				addField("signatory_name", "");
+				addField("cts", "");
+				addField("end_date", "");
+				addField("date_tbc", "");
+				addField("hours_of_work", "");
+				addField("travel_supplement_start", "");
+				addField("travel_supplement_duration", "");
+				addField("pence_per_mile", "");
+				addField("personal_qualification", "");
+				
+				//add data from document, basing on which row we're in now.
 				switch(rowCount){
 					case 2: addField("start_date", cells.get(1).getText());
 							break;
@@ -60,61 +78,70 @@ public class DocxHireModel extends HireAbstractModel {
 							break;
 					case 14: addField("postal_code", cells.get(3).getText());
 							break;	
-					case 15: addField("country", cells.get(3).getText());
+					case 15: 
+							if(cells.get(3).getText().substring(cells.get(3).getText().length() - 1).equals(" ")){
+								addField("country", cells.get(3).getText().substring(0, cells.get(3).getText().length()-1));
+							} else {
+								addField("country", cells.get(3).getText());
+							}
 							break;
-					case 20: addField("salary", cells.get(3).getText());
+					case 20: addField("salary", getSalary(cells.get(3).getText()));
 							break;
-					case 23: addField("addWagType1", cells.get(0).getText());
-							addField("addWageTypeAmount1", cells.get(1).getText());
+					case 23: addField("addWageType1", cells.get(0).getText());
+							addField("addWageTypeAmount1", getSalary(cells.get(1).getText()));
 							addField("addWageType2", cells.get(2).getText());
-							addField("addWageTypeAmount2", cells.get(3).getText());
+							addField("addWageTypeAmount2", getSalary(cells.get(3).getText()));
 							break;
 					case 25: addField("lm_name", cells.get(1).getText());
 							break;
 					case 26: addField("lm_phone_no", cells.get(1).getText());
 							//Find if there's probation period
-							if(cells.get(3).getText() == "Yes"){
-								addField("probation_period", "True");
+							if(cells.get(3).getText().equals("Yes")){
+								addField("probation_period", "true");
 							} else {
-								addField("probation_period", "False");
+								addField("probation_period", "false");
 							}
-					
 							break;
 					case 27: 
 							//Find if competition compliance paragraph is needed
-							if(cells.get(1).getText() == "Yes"){
-								addField("competition_compliance", "True");
+							if(cells.get(1).getText().equals("Yes")){
+								addField("competition_compliance", "true");
 							} else {
-								addField("competition_compliance", "False");
+								addField("competition_compliance", "false");
 							}
 							addField("duration_of_probation", cells.get(3).getText());
 							break;
 					case 28: 
 							//Find in there's a sign on bonus
-							if(cells.get(1).getText() == "Yes"){
-								addField("sign_on_bonus", "True");
+							if(cells.get(1).getText().equals("Yes")){
+								addField("sign_on_bonus", "true");
 							} else {
-								addField("sign_on_bonus", "False");
+								addField("sign_on_bonus", "false");
 							}
 							//Find in there's a company credit card
-							if(cells.get(3).getText() == "Yes"){
-								addField("company_credit_card", "True");
+							if(cells.get(3).getText().equals("Yes")){
+								addField("company_credit_card", "true");
 							} else {
-								addField("company_credit_card", "False");
+								addField("company_credit_card", "false");
 							}
 							break;
-					case 29: addField("sign_on_bonus_amount", cells.get(1).getText());
-							addField("travel_supplement", cells.get(3).getText());
+					case 29: addField("sign_on_bonus_amount", getSalary(cells.get(1).getText()));
+							if(cells.get(3).getText().toLowerCase().equals("yes")){
+								addField("travel_supplement", "true");
+							} else {
+								addField("travel_supplement", "false");
+							}
 							break;
 					case 30: 
 							//Find in there's a relocation
-							if(cells.get(1).getText() == "Yes"){
-								addField("relocation", "True");
+							if(cells.get(1).getText().equals("Yes")){
+								addField("relocation", "true");
 							} else {
-								addField("relocation", "False");
+								addField("relocation", "false");
 							}
+							addField("travel_supplement_amount", getSalary(cells.get(3).getText()));
 							break;
-					case 31: addField("relocation_amount", cells.get(1).getText());
+					case 31: addField("relocation_amount", getSalary(cells.get(1).getText()));
 							//Find correct company car value
 							if(cells.get(3).getText().toLowerCase().contains("company car") && !cells.get(3).getText().toLowerCase().contains("cash")){
 								addField("company_car", "Company Car");
@@ -128,14 +155,18 @@ public class DocxHireModel extends HireAbstractModel {
 					
 							break;
 					case 32: addField("relocation_area", cells.get(1).getText());
-							addField("mobile_phone", cells.get(3).getText());
+							if(cells.get(3).getText().toLowerCase().equals("yes")){
+								addField("mobile_phone", "true");
+							} else {
+								addField("mobile_phone", "false");
+							}
 							break;
 					case 33: 
 							//Find if there are professional subscriptions
-							if(cells.get(1).getText() == "Yes"){
-								addField("professional_subs", "True");
+							if(cells.get(1).getText().equals("Yes")){
+								addField("professional_subs", "true");
 							} else {
-								addField("professional_subs", "False");
+								addField("professional_subs", "false");
 							}
 							break;
 					case 35: addField("next_salary_review", cells.get(1).getText());
@@ -150,16 +181,32 @@ public class DocxHireModel extends HireAbstractModel {
 						break;
 					case 39: 
 							//Find if working visa paragraph is needed
-							if(cells.get(1).getText() == "No" || cells.get(1).getText() == ""){
-								addField("working_visa_paragraph", "False");
+							if(cells.get(1).getText().equals("No") || cells.get(1).getText().equals("")){
+								addField("working_visa_paragraph", "false");
 							} else {
-								addField("working_visa_paragraph", "True");
+								addField("working_visa_paragraph", "true");
 							}
 						break;	
 				}
 			}
 		}
 
+	}
+	
+	private String getSalary(String salary){
+		Pattern pattern = Pattern.compile("(\\d{0,3},{0,1}\\d{3}.{0,1}\\d{0,2})");
+		Matcher match = pattern.matcher(salary);
+		Boolean found = false;
+		while (match.find()) {
+			salary = match.group();
+			found = true;
+		}
+		
+		if(!found){
+			return "";
+		} else {
+			return salary;
+		}
 	}
 
 	@Override
