96a12cbcbe7177061afda9c37cb1244b868dbf38
Gorah <bartosz.kratochwil@hp.com>
2014-04-09 16:17:27 +0200

    added function to trim trailing spaces

--- contract/model/DocxHireModel.java
+++ contract/model/DocxHireModel.java
@@ -53,50 +53,45 @@ public class DocxHireModel extends HireAbstractModel {
 				
 				//add data from document, basing on which row we're in now.
 				switch(rowCount){
-					case 2: addField("start_date", cells.get(1).getText());
+					case 2: addField("start_date", filterTrailingSpaces(cells.get(1).getText()));
 							break;
-					case 3: addField("position_number", cells.get(1).getText());
-							addField("position_title", cells.get(3).getText());
+					case 3: addField("position_number", filterTrailingSpaces(cells.get(1).getText()));
+							addField("position_title", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 4:	addField("work_contract", cells.get(1).getText());
+					case 4:	addField("work_contract", filterTrailingSpaces(cells.get(1).getText()));
 							break;
-					case 5:	addField("contract_type", cells.get(1).getText());
-							addField("contract_end_date", cells.get(3).getText());
+					case 5:	addField("contract_type", filterTrailingSpaces(cells.get(1).getText()));
+							addField("contract_end_date", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 6: addField("job_grade", cells.get(1).getText());
+					case 6: addField("job_grade", filterTrailingSpaces(cells.get(1).getText()));
 							break;
-					case 8: addField("mcbc_sharps", cells.get(1).getText());
-							addField("business_area", cells.get(3).getText());
+					case 8: addField("mcbc_sharps", filterTrailingSpaces(cells.get(1).getText()));
+							addField("business_area", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 11: addField("forenames", cells.get(1).getText());
-							addField("address_line1", cells.get(3).getText());
+					case 11: addField("forenames", filterTrailingSpaces(cells.get(1).getText()));
+							addField("address_line1", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 12: addField("surname", cells.get(1).getText());
-							addField("address_line2", cells.get(3).getText());
+					case 12: addField("surname", filterTrailingSpaces(cells.get(1).getText()));
+							addField("address_line2", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 13: addField("city", cells.get(3).getText());
+					case 13: addField("city", filterTrailingSpaces(cells.get(3).getText()));
 							break;
-					case 14: addField("postal_code", cells.get(3).getText());
+					case 14: addField("postal_code", filterTrailingSpaces(cells.get(3).getText()));
 							break;	
-					case 15: 
-							if(cells.get(3).getText().substring(cells.get(3).getText().length() - 1).equals(" ")){
-								addField("country", cells.get(3).getText().substring(0, cells.get(3).getText().length()-1));
-							} else {
-								addField("country", cells.get(3).getText());
-							}
+					case 15: addField("country", filterTrailingSpaces(cells.get(3).getText()));
 							break;
 					case 20: addField("salary", getSalary(cells.get(3).getText()));
 							break;
-					case 23: addField("addWageType1", cells.get(0).getText());
+					case 23: addField("addWageType1", filterTrailingSpaces(cells.get(0).getText()));
 							addField("addWageTypeAmount1", getSalary(cells.get(1).getText()));
-							addField("addWageType2", cells.get(2).getText());
+							addField("addWageType2", filterTrailingSpaces(cells.get(2).getText()));
 							addField("addWageTypeAmount2", getSalary(cells.get(3).getText()));
 							break;
-					case 25: addField("lm_name", cells.get(1).getText());
+					case 25: addField("lm_name", filterTrailingSpaces(cells.get(1).getText()));
 							break;
-					case 26: addField("lm_phone_no", cells.get(1).getText());
+					case 26: addField("lm_phone_no", filterTrailingSpaces(cells.get(1).getText()));
 							//Find if there's probation period
-							if(cells.get(3).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(3).getText()).equals("Yes")){
 								addField("probation_period", "true");
 							} else {
 								addField("probation_period", "false");
@@ -104,29 +99,29 @@ public class DocxHireModel extends HireAbstractModel {
 							break;
 					case 27: 
 							//Find if competition compliance paragraph is needed
-							if(cells.get(1).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(1).getText()).equals("Yes")){
 								addField("competition_compliance", "true");
 							} else {
 								addField("competition_compliance", "false");
 							}
-							addField("duration_of_probation", cells.get(3).getText());
+							addField("duration_of_probation", filterTrailingSpaces(cells.get(3).getText()));
 							break;
 					case 28: 
 							//Find in there's a sign on bonus
-							if(cells.get(1).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(1).getText()).equals("Yes")){
 								addField("sign_on_bonus", "true");
 							} else {
 								addField("sign_on_bonus", "false");
 							}
 							//Find in there's a company credit card
-							if(cells.get(3).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(3).getText()).equals("Yes")){
 								addField("company_credit_card", "true");
 							} else {
 								addField("company_credit_card", "false");
 							}
 							break;
 					case 29: addField("sign_on_bonus_amount", getSalary(cells.get(1).getText()));
-							if(cells.get(3).getText().toLowerCase().equals("yes")){
+							if(filterTrailingSpaces(cells.get(3).getText()).toLowerCase().equals("yes")){
 								addField("travel_supplement", "true");
 							} else {
 								addField("travel_supplement", "false");
@@ -134,7 +129,7 @@ public class DocxHireModel extends HireAbstractModel {
 							break;
 					case 30: 
 							//Find in there's a relocation
-							if(cells.get(1).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(1).getText()).equals("Yes")){
 								addField("relocation", "true");
 							} else {
 								addField("relocation", "false");
@@ -154,8 +149,8 @@ public class DocxHireModel extends HireAbstractModel {
 							}
 					
 							break;
-					case 32: addField("relocation_area", cells.get(1).getText());
-							if(cells.get(3).getText().toLowerCase().equals("yes")){
+					case 32: addField("relocation_area", filterTrailingSpaces(cells.get(1).getText()));
+							if(filterTrailingSpaces(cells.get(3).getText()).toLowerCase().equals("yes")){
 								addField("mobile_phone", "true");
 							} else {
 								addField("mobile_phone", "false");
@@ -163,17 +158,17 @@ public class DocxHireModel extends HireAbstractModel {
 							break;
 					case 33: 
 							//Find if there are professional subscriptions
-							if(cells.get(1).getText().equals("Yes")){
+							if(filterTrailingSpaces(cells.get(1).getText()).equals("Yes")){
 								addField("professional_subs", "true");
 							} else {
 								addField("professional_subs", "false");
 							}
 							break;
-					case 35: addField("next_salary_review", cells.get(1).getText());
+					case 35: addField("next_salary_review", filterTrailingSpaces(cells.get(1).getText()));
 						break;
-					case 36: addField("non_negotiated", cells.get(1).getText());
+					case 36: addField("non_negotiated", filterTrailingSpaces(cells.get(1).getText()));
 						break;
-					case 37: addField("reason_for_contract", cells.get(1).getText());
+					case 37: addField("reason_for_contract", filterTrailingSpaces(cells.get(1).getText()));
 						break;
 					case 38: 
 				
@@ -181,7 +176,7 @@ public class DocxHireModel extends HireAbstractModel {
 						break;
 					case 39: 
 							//Find if working visa paragraph is needed
-							if(cells.get(1).getText().equals("No") || cells.get(1).getText().equals("")){
+							if(filterTrailingSpaces(cells.get(1).getText()).equals("No") || cells.get(1).getText().equals("")){
 								addField("working_visa_paragraph", "false");
 							} else {
 								addField("working_visa_paragraph", "true");
@@ -207,6 +202,11 @@ public class DocxHireModel extends HireAbstractModel {
 		} else {
 			return salary;
 		}
+	}
+	
+	private String filterTrailingSpaces(String text){
+		String result = text.replaceAll("\\s+$", "");
+		return result;
 	}
 
 	@Override
