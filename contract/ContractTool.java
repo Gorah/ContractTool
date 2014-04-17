e125a1aa6aae7a684336383b889517caa1464ebb
Gorah <bartosz.kratochwil@hp.com>
2014-04-17 12:27:20 +0200

    changed program starter to include combo items repository

--- contract/ContractTool.java
+++ contract/ContractTool.java
@@ -1,5 +1,17 @@
 package contract;
 
-public class ContractTool {
+import contract.repository.ComboOptions;
 
+public class ContractTool {
+	private ComboOptions comboOptions;
+
+	public ContractTool(ComboOptions options){
+		this.comboOptions = options;
+	}
+
+	public ComboOptions getComboOptions() {
+		return comboOptions;
+	}
+	
+	
 }
--- contract/Starter.java
+++ contract/Starter.java
@@ -1,19 +1,27 @@
 package contract;
 
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.microsoft.sqlserver.jdbc.SQLServerConnectionPoolDataSource;
+
 import contract.controller.AppController;
 import contract.gui.view.swing.Main;
 import contract.gui.view.swing.NewHire;
 import contract.gui.view.swing.OpenContract;
+import contract.logging.ContractLogger;
+import contract.model.Settings;
+import contract.repository.JDBCOptionsRepository;
 
 public class Starter {
 
 	public static void main(String[] args) {
+		Logger logger = new ContractLogger(Starter.class.getName()).getLogger();
+		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException
@@ -22,13 +30,23 @@ public class Starter {
 				.log(Level.WARNING, "Invalid look and feel class");
 		}
 		
-		ContractTool tool = new ContractTool();
-		AppController app = new AppController(tool);
-		app.addViews(new Main(),
-					new OpenContract(),
-					new NewHire());
-		app.init();
-	
+		SQLServerConnectionPoolDataSource dataSource = new SQLServerConnectionPoolDataSource();
+		dataSource.setURL("jdbc:" + Settings.DBTYPE + "://BPOPLMCBC16;databaseName=" + Settings.DB_NAME);
+		dataSource.setUser(Settings.DB_USER);
+		dataSource.setPassword(Settings.DB_PASS);
+		
+		ContractTool tool;
+		try {
+			tool = new ContractTool(new JDBCOptionsRepository(dataSource));
+			AppController app = new AppController(tool);
+			app.addViews(new Main(),
+						new OpenContract(),
+						new NewHire());
+			app.init();
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE, "Cannot initialise program. " + e.getMessage());
+			System.exit(0);
+		}
 	}
 
 }
